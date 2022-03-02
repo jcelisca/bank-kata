2ad4268fd6548bd78fd20524c8ead647e1e68977
jcelisca <celiss1997@gmail.com>
2022-03-02 14:57:30 -0500

    Creación de una clase para dar formato de fecha a un string

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,4 +1,12 @@
+import domain.DateFormat;
+
 public class Account {
+
+    public static void main(String[] args) {
+        DateFormat date = new DateFormat();
+        System.out.println(date.date("02/07/2022"));
+    }
+
 
     void deposit(int amount) {
 
---
+++ src/main/java/domain/DateFormat.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormat {
+
+    public LocalDate date(String currentDate){
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(currentDate, format);
+        return date;
+    }
+}
